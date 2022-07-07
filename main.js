9fc4105f56e8578f311f620ec7df77de9ea8cde4
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-07-07 11:27:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setup() {
     canvas = createCanvas(600, 480);
-    canvas.position(110, 250);
+    canvas.position(375, 300);
     video = createCapture(VIDEO);
     video.hide();
 
@@ -8,8 +8,29 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 600, 480);
+    image(video, 160, 90, 300, 300);
     tint(tint_color);
+
+    stroke(0, 255, 100);
+    fill(255, 255, 255, 0);
+    rect(40, 40, 520, 400);
+
+    fill(0, 240, 230);
+    stroke(0, 240, 230);
+    circle(40, 40, 65);
+
+    fill(0, 240, 230);
+    stroke(0, 240, 230);
+    circle(40, 440, 65);
+
+    fill(0, 240, 230);
+    stroke(0, 240, 230);
+    circle(560, 440, 65);
+    
+    fill(0, 240, 230);
+    stroke(0, 240, 230);
+    circle(560, 50, 65);
+
 }
 
 function apply() {
